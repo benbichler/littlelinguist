45be9bb92d0bdd76d5e22a73b720c8b70b503dc5
Ben Bichler <benbichler2@gmail.com>
2024-05-12 18:29:50 +0300

    timer component creation

---
+++ src/app/timer/timer.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-timer',
+  standalone: true,
+  imports: [],
+  templateUrl: './timer.component.html',
+  styleUrl: './timer.component.css',
+})
+export class TimerComponent {}
