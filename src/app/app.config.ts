e551c2775d6f90335b44b8f83d729d6e9b6f6892
benbichler <benbichler2@gmail.com>
2024-05-22 10:55:57 +0300

    attempt 1 to fix

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,23 +3,11 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes, withComponentInputBinding()),
     provideAnimations(),
-    provideFirebaseApp(() =>
-      initializeApp({
-        projectId: 'little-linguist-1236f',
-        appId: '1:1059238954258:web:4bd85aaaab30a4b553dfcb',
-        storageBucket: 'little-linguist-1236f.appspot.com',
-        apiKey: 'AIzaSyAquK-On98FdXgbVs1tPIhi3W9bP82aiII',
-        authDomain: 'little-linguist-1236f.firebaseapp.com',
-        messagingSenderId: '1059238954258',
-      })
-    ),
-    provideFirestore(() => getFirestore()),
   ],
 };
